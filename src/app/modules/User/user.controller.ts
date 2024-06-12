51cce17436c6b3b96894995a497e8c0f5d13ba82
naimuddin94 <developernaimuddin@gmail.com>
2024-06-12 12:42:13 +0600

    feat: create user.controller.ts

---
+++ src/app/modules/User/user.controller.ts
@@ -0,0 +1,15 @@
+import { ApiResponse, asyncHandler } from '../../utils';
+import { UserService } from './user.service';
+
+const createUser = asyncHandler(async (req, res) => {
+  const userData = req.body;
+  const result = await UserService.saveUserIntoDB(userData);
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, result, 'User created successfully'));
+});
+
+export const UserController = {
+  createUser,
+};
