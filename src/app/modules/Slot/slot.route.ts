dc8884b74a2e9f8026fc475c3f2f25505e2cfce8
naimuddin94 <developernaimuddin@gmail.com>
2024-06-13 17:02:47 +0600

    create slot route

---
+++ src/app/modules/Slot/slot.route.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import auth from '../../middleware/auth';
+import validateRequest from '../../middleware/validateRequest';
+import { USER_ROLE } from '../User/user.constant';
+import { SlotController } from './slot.controller';
+import { SlotValidation } from './slot.validation';
+
+const router = Router();
+
+router
+  .route('/')
+  .post(
+    auth(USER_ROLE.admin),
+    validateRequest(SlotValidation.createSlotValidationSchema),
+    SlotController.createSlot,
+  );
+
+export const slotRouter = router;
